5c131a6509ea50a20f152493fa025c9255019333
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-06-12 16:30:43 -0300

    finished drinks endpoints

--- drinks/views.py
+++ drinks/views.py
@@ -5,20 +5,22 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-#func that handle the get and post for drink list
+
+# func that handle the get and post for drink list
 @api_view(['GET', 'POST'])
 def drink_list(request):
     if request.method == 'GET':
         drinks = Drink.objects.all()
         serializer = DrinkSerializer(drinks, many=True)
-        return JsonResponse({ 'drinks': serializer.data })
+        return Response(serializer.data)
     if request.method == 'POST':
         serializer = DrinkSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-#handling these api_view requests for details
+
+# handling these api_view requests for details
 @api_view(['GET', 'PUT', 'DELETE'])
 def drink_detail(request, id):
     try:
@@ -36,6 +38,5 @@ def drink_detail(request, id):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
-        pass
-
-
+        drink.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
