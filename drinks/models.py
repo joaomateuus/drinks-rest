14b3cb878ba17091005e390ca0176488123955f7
Joao Mateus <joao.amaral@ifam.edu.br>
2022-09-22 21:49:47 -0300

    feat: adding some features

--- drinks/models.py
+++ drinks/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-"""
 from django.utils.translation import gettext_lazy as _
 
 class Category(models.Model):
@@ -9,19 +8,29 @@ class Category(models.Model):
         DESTILATE = 3, _('Destilate')
         WINES = 4, _('Wines')
         
-    category = models.CharField(
-        choices = DrinkCategory.choices(max_lenght = 50)
-    )
+    category = models.CharField(max_length = 40, choices = DrinkCategory.choices)
     
     def __str__(self):
         return self.category 
-"""
-        
 
 class Drink(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
-    # category = models.ForeignKey(Category)
-
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+    quantidade_liquido_bebida = models.FloatField(null=True, blank=True)
+    
     def __str__(self):
         return self.name
+
+class CalculoLitros(models.Model):
+    unidades = models.IntegerField()
+    quantidade_liquido = models.FloatField
+    total_litros = models.FloatField()
+    
+    def calculando_litros(self):
+        self.total_litros = self.unidades * self.quantidade_liquido
+        return self.total_litros
+        
+    
+    
